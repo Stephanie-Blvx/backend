93389ca51e43a0f48f0cc3f0bff0c51ad0d41d73
stéphanie <sbelveaux@gmail.com>
2024-09-20 14:59:28 +0200

    back

--- routes/year.js
+++ routes/year.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 
 router.get('/', (req, res) => {
- const year =  new Date().getFullYear();
+ const year =  new Date().getFullYear().toString();
  res.json({ now: year });
 });
 
 
 module.exports = router
+(new Date().getFullYear().toString())
